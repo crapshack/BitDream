29af60011c775046ad17b87823f8fe332d6a0561
austin-smith <austinsmith23@gmail.com>
2025-03-08 14:29:57 -0800

    Remove old duplicate ContentView

--- BitDream/Views/ContentView.swift
+++ BitDream/Views/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Foundation
 import KeychainAccess
 
-struct ContentViewOrig: View {
+struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         entity: Host.entity(),
@@ -20,154 +20,173 @@ struct ContentViewOrig: View {
 
     private var keychain = Keychain(service: "crapshack.BitDream")
 
+    @State var torrentSelection: Torrent?
     @State var sortBySelection: sortBy = .name
     @State var filterBySelection: [TorrentStatusCalc] = TorrentStatusCalc.allCases
 
     var body: some View {
-        NavigationStack {
-            Spacer()
+        NavigationSplitView {
+       // } content: {
             VStack {
                 Divider()
                 HStack {
                     Text(String("\(store.sessionStats?.activeTorrentCount ?? 0) active dreams"))
                     Spacer()
-                    Text(String("▲ \(byteCountFormatter.string(fromByteCount: store.sessionStats?.uploadSpeed ?? 0))/s"))
                     Text(String("▼ \(byteCountFormatter.string(fromByteCount: store.sessionStats?.downloadSpeed ?? 0))/s"))
+                    Text(String("▲ \(byteCountFormatter.string(fromByteCount: store.sessionStats?.uploadSpeed ?? 0))/s"))
 
                 }
-                .foregroundColor(.secondary)
                 .font(.subheadline)
                 .padding([.leading, .trailing])
                 Divider()
             }
-
             VStack {
                 List {
+                    #if os(iOS)
                     ForEach(sortTorrents(store.torrents.filter() {filterBySelection.contains([$0.statusCalc])}, sortBy: sortBySelection), id:\.id) { torrent in
                         NavigationLink(destination: TorrentDetail(store: store, viewContext: viewContext, torrent: binding(for: torrent))) {
                             TorrentListRow(torrent: binding(for: torrent), store: store)
                         }
                     }
-                }
-                .listRowSeparator(.automatic)
-                .listStyle(PlainListStyle())
-                .clipped()
-                .refreshable {
-                    updateList(store: store, update: {_ in})
-                }
-                .onAppear(perform: {
-                    hosts.forEach { h in
-                        if (h.isDefault) {
-                            store.setHost(host: h)
+                    #else
+                    ForEach(sortTorrents(store.torrents.filter() {filterBySelection.contains([$0.statusCalc])}, sortBy: sortBySelection), id:\.id) { torrent in
+                        NavigationLink(destination: TorrentDetail(store: store, viewContext: viewContext, torrent: binding(for: torrent))) {
+                            TorrentListRow(torrent: binding(for: torrent), store: store)
+                                .contentShape(Rectangle())
+                                .onTapGesture {
+                                    torrentSelection = torrent
+                                }
+                                .listRowSeparator(.hidden)
                         }
+                        .listRowSeparator(.visible)
+                      //      .listRowBackground(torrent == torrentSelection ? Color.accentColor.opacity(0.5) : Color(.clear))
+                        .listRowBackground(
+                            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                .fill(torrent == torrentSelection ? Color.accentColor.opacity(0.5) : Color(.clear))
+                                .padding(.leading, 12)
+                        )
                     }
-                    if (store.host != nil) {
-                        store.startTimer()
-                    } else {
-                        // Create a new host
-                        store.setup = true
+                    #endif
+                }
+                .padding(.trailing, 12)
+            }
+            #if os(iOS)
+            .listStyle(PlainListStyle())
+            #endif
+            .refreshable {
+                updateList(store: store, update: {_ in})
+            }
+            .onAppear(perform: {
+                hosts.forEach { h in
+                    if (h.isDefault) {
+                        store.setHost(host: h)
                     }
-                })
-#if os(iOS)
-                .navigationBarTitle(Text("Dreams"), displayMode: .inline)
-#else
-                .navigationTitle("Dreams")
-#endif
-                .toolbar {
-                    ToolbarItem(placement: .automatic) {
+                }
+                if (store.host != nil) {
+                    store.startTimer()
+                } else {
+                    // Create a new host
+                    store.setup = true
+                }
+            })
+            #if os(iOS)
+            .navigationBarTitle(Text("Dreams"), displayMode: .inline)
+            #endif
+            .toolbar {
+                ToolbarItem(placement: .automatic) {
+                    Menu {
                         Menu {
-                            Menu {
-                                ForEach(hosts, id: \.self) { host in
-                                    Button(action: {
-                                        store.setHost(host: host)
-                                    }) {
-                                        Label(host.name!, systemImage: host == store.host ? "checkmark" : "")
-                                    }
-
+                            ForEach(hosts, id: \.self) { host in
+                                Button(action: {
+                                    store.setHost(host: host)
+                                }) {
+                                    Label(host.name!, systemImage: host == store.host ? "checkmark" : "")
                                 }
 
-                            } label: {
-                                Label("Server", systemImage: "arrow.triangle.2.circlepath")
                             }
 
-                            Divider()
+                        } label: {
+                            Label("Server", systemImage: "arrow.triangle.2.circlepath")
+                        }
 
-                            Button(action: {store.setup.toggle()}) {
-                                Label("Add", systemImage: "plus")
+                        Divider()
+
+                        Button(action: {store.setup.toggle()}) {
+                            Label("Add", systemImage: "plus")
+                        }
+                        Button(action: {store.editServers.toggle()}) {
+                            Label("Edit", systemImage: "square.and.pencil")
+                        }
+                    } label: {
+                        Image(systemName: "server.rack")
+                    }
+                }
+                ToolbarItemGroup (placement: .automatic) {
+                    Menu {
+                        Menu {
+                            Section(header: Text("Include")) {
+                                Button("All") {
+                                    filterBySelection = TorrentStatusCalc.allCases
+                                }
+                                Button("Downloading") {
+                                    filterBySelection = [.downloading]
+                                }
+                                Button("Complete") {
+                                    filterBySelection = [.complete]
+                                }
+                                Button("Paused") {
+                                    filterBySelection = [.paused]
+                                }
                             }
-                            Button(action: {store.editServers.toggle()}) {
-                                Label("Edit", systemImage: "square.and.pencil")
+                            Section(header: Text("Exclude")) {
+                                Button("Complete") {
+                                    filterBySelection = TorrentStatusCalc.allCases.filter {$0 != .complete}
+                                }
                             }
                         } label: {
-                            Image(systemName: "server.rack")
-                        }
-                    }
-                    ToolbarItemGroup (placement: .automatic) {
+                            Text("Filter By")
+                            Image(systemName: "slider.horizontal.3")
+                        }.environment(\.menuOrder, .fixed)
+                        
                         Menu {
-                            Menu {
-                                Section(header: Text("Include")) {
-                                    Button("All") {
-                                        filterBySelection = TorrentStatusCalc.allCases
-                                    }
-                                    Button("Downloading") {
-                                        filterBySelection = [.downloading]
-                                    }
-                                    Button("Complete") {
-                                        filterBySelection = [.complete]
-                                    }
-                                    Button("Paused") {
-                                        filterBySelection = [.paused]
-                                    }
+                            Picker("Sort By", selection: $sortBySelection) {
+                                ForEach(sortBy.allCases, id: \.self) { item in
+                                    Text(item.rawValue)
                                 }
-                                Section(header: Text("Exclude")) {
-                                    Button("Complete") {
-                                        filterBySelection = TorrentStatusCalc.allCases.filter {$0 != .complete}
-                                    }
-                                }
-                            } label: {
-                                Text("Filter By")
-                                Image(systemName: "slider.horizontal.3")
-                            }.environment(\.menuOrder, .fixed)
-                            
-                            Menu {
-                                Picker("Sort By", selection: $sortBySelection) {
-                                    ForEach(sortBy.allCases, id: \.self) { item in
-                                        Text(item.rawValue)
-                                    }
-                                }
-                            } label: {
-                                Text("Sort By")
-                                Image(systemName: "arrow.up.arrow.down")
-                            }.environment(\.menuOrder, .fixed)
-                            
-                            Divider()
-                            
-                            Button(action: {
-                                playPauseAllTorrents(start: false, info: makeConfig(store: store), onResponse: { response in
-                                    updateList(store: store, update: {_ in})
-                                })
-                            }) {
-                                Label("Pause All", systemImage: "pause")
-                            }
-                            
-                            Button(action: {
-                                playPauseAllTorrents(start: true, info: makeConfig(store: store), onResponse: { response in
-                                    updateList(store: store, update: {_ in})
-                                })
-                            }) {
-                                Label("Resume All", systemImage: "play")
-                            }
-                            
-                            Divider()
-                            
-                            Button(action: {
-                                store.isShowingAddAlert.toggle()
-                            }) {
-                                Label("Add Torrent", systemImage: "plus")
+                                .pickerStyle(.automatic)
                             }
                         } label: {
-                            Image(systemName: "ellipsis.circle")
+                            Text("Sort By")
+                            Image(systemName: "arrow.up.arrow.down")
+                        }.environment(\.menuOrder, .fixed)
+                        
+                        Divider()
+                        
+                        Button(action: {
+                            playPauseAllTorrents(start: false, info: makeConfig(store: store), onResponse: { response in
+                                updateList(store: store, update: {_ in})
+                            })
+                        }) {
+                            Label("Pause All", systemImage: "pause")
                         }
+                        
+                        Button(action: {
+                            playPauseAllTorrents(start: true, info: makeConfig(store: store), onResponse: { response in
+                                updateList(store: store, update: {_ in})
+                            })
+                        }) {
+                            Label("Resume All", systemImage: "play")
+                        }
+                        
+                        Divider()
+                        
+                        Button(action: {
+                            store.isShowingAddAlert.toggle()
+                        }) {
+                            Label("Add Torrent", systemImage: "plus")
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
                     }
                 }
             }
@@ -208,6 +227,14 @@ struct ContentViewOrig: View {
                 //                    store.isError.toggle()
                 //                })
             })
+            
+        } detail: {
+            if let torrentSelection = torrentSelection {
+                TorrentDetail(store: store, viewContext: viewContext, torrent: binding(for: torrentSelection))
+            }
+            else {
+                Text("No Selection")
+            }
         }
     }
 
